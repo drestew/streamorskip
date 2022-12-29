d173f7ce7d0871206916b32eadb20ee0dd96d1de
drestew <drestew48@gmail.com>
2022-12-29 02:17:11 -0500

    Get data fetch working from supabase

--- pages/api/updateMediaCatalog.ts
+++ pages/api/updateMediaCatalog.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from '../../utils/supabase';
 
 type unogsResponse = {
   method: string;
@@ -18,14 +19,21 @@ const options: unogsResponse = {
 
 const getNewMedia = async () => {
   const mediaData = await fetch(
-    'https://unogsng.p.rapidapi.com/search?start_year=1972&orderby=rating&audiosubtitle_andor=and&limit=3&subtitle=english&countrylist=78%2C46&audio=english&country_andorunique=unique&offset=0&end_year=2019',
+    'https://unogsng.p.rapidapi.com/search?start_year=1972&orderby=rating&audiosubtitle_andor=and&limit=1&subtitle=english&countrylist=78%2C46&audio=english&country_andorunique=unique&offset=0&end_year=2019',
     options
   );
   return mediaData.json();
 };
 
+const addMediaToDB = () => {
+  return supabase
+    .from('media')
+    .insert({ nfid: 46, title: 'second title movie' })
+    .select();
+};
+
 const newMedia = async (req: NextApiRequest, res: NextApiResponse) => {
-  const mediaData = await getNewMedia();
+  const mediaData = await addMediaToDB();
   res.json(mediaData);
 };
 
---
+++ types/supabase.ts
@@ -0,0 +1,64 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[];
+
+export interface Database {
+  public: {
+    Tables: {
+      media: {
+        Row: {
+          id: number;
+          created_at: string | null;
+          title: string | null;
+          synopsis: string | null;
+          img: string | null;
+          vtype: string | null;
+          nfid: number;
+          poster: string | null;
+          titledate: string | null;
+          on_Nflix: boolean | null;
+          year: number | null;
+        };
+        Insert: {
+          id?: number;
+          created_at?: string | null;
+          title?: string | null;
+          synopsis?: string | null;
+          img?: string | null;
+          vtype?: string | null;
+          nfid: number;
+          poster?: string | null;
+          titledate?: string | null;
+          on_Nflix?: boolean | null;
+          year?: number | null;
+        };
+        Update: {
+          id?: number;
+          created_at?: string | null;
+          title?: string | null;
+          synopsis?: string | null;
+          img?: string | null;
+          vtype?: string | null;
+          nfid?: number;
+          poster?: string | null;
+          titledate?: string | null;
+          on_Nflix?: boolean | null;
+          year?: number | null;
+        };
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+  };
+}

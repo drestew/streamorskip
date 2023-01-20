440ced059e9ccc287e0afa3d24bf85d5792f31b9
drestew <drestew48@gmail.com>
2023-01-19 21:15:27 -0500

    Add genres to db

--- pages/api/cron/genre.ts
+++ pages/api/cron/genre.ts
@@ -2,6 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { supabaseService } from '../../../utils/supabase';
 import { options } from '../../../utils/unogs';
 
+type Genre = {
+  nfid: number;
+  genres: [
+    {
+      nfid: number;
+      genre: string;
+    }
+  ];
+};
+
 const getCatalogFromDB = async () => {
   const { data, error } = await supabaseService.from('catalog').select('nfid');
 
@@ -17,15 +27,16 @@ const getCatalogFromDB = async () => {
 
 const getGenres = async () => {
   const catalog = await getCatalogFromDB();
-  let genres;
+  let genres: Genre[] = [];
 
   if (catalog) {
-    genres = Promise.all(
+    genres = await Promise.all(
       catalog.map(async (item) => {
         const url = `https://unogsng.p.rapidapi.com/titlegenres?netflixid=${item.nfid}`;
         const getGenres = await fetch(url, options);
+        const { results } = await getGenres.json();
 
-        return getGenres.json();
+        return { nfid: item.nfid, genres: results };
       })
     );
   }
@@ -33,9 +44,35 @@ const getGenres = async () => {
   return genres;
 };
 
+const addGenresToDB = async () => {
+  const genres: Genre[] = await getGenres();
+
+  if (genres) {
+    for (let i = 0; i < genres.length; i++) {
+      const { error } = await supabaseService.from('catalog-genre').insert(
+        genres[i].genres.map((item) => {
+          return {
+            ['catalog-nfid']: genres[i].nfid,
+            genre: item.genre,
+            ['genre-nfid']: item.nfid,
+          };
+        })
+      );
+
+      if (error) {
+        console.log('Error:', {
+          message: error.message,
+          details: error.details,
+        });
+      }
+    }
+  }
+};
+
 const apiResponse = async (req: NextApiRequest, res: NextApiResponse) => {
-  const result = await getGenres();
+  const result = await addGenresToDB();
   res.json(result);
+  ``;
 };
 
 export default apiResponse;

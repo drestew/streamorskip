5b7fa30c3789c4daa94f8a22667f68c51890131e
drestew <drestew48@gmail.com>
2023-02-24 00:30:12 -0500

    Add utility type for step functions

--- pages/api/cron/inngestFunctions/rating.ts
+++ pages/api/cron/inngestFunctions/rating.ts
@@ -9,6 +9,7 @@ import { Inngest } from 'inngest';
 type NeedRating = Awaited<ReturnType<typeof getNullRatingsFromDB>>;
 
 const limiter = new Bottleneck({ minTime: 300 });
+const limiterImdbId = new Bottleneck({ minTime: 600 });
 const inngest = new Inngest({ name: 'streamorskip' });
 
 export default inngest.createFunction(
@@ -41,7 +42,9 @@ const getNullRatingsFromDB = async () => {
   const { data, error } = await supabaseService
     .from('catalog')
     .select('title, imdbid, rating')
-    .or('rating.is.null, rating.eq.0');
+    .or('rating.is.null, rating.eq.0')
+    .order('id', { ascending: false })
+    .range(0, 100);
 
   if (error) {
     console.log('Error:', {
@@ -49,55 +52,63 @@ const getNullRatingsFromDB = async () => {
       details: error.details,
     });
   }
-
+  console.log(data);
   return data;
 };
 
 const addImdbIdsToDB = async (itemsWithImdbIds: ImdbIdItem[]) => {
-  itemsWithImdbIds.map(async (item) => {
-    const { error } = await supabaseService
-      .from('catalog')
-      .update({
-        imdbid: item.imdbid,
-      })
-      .eq('title', item.title);
+  itemsWithImdbIds.length > 0 &&
+    itemsWithImdbIds.map(async (item) => {
+      const { error } = await supabaseService
+        .from('catalog')
+        .update({
+          imdbid: item.imdbid,
+        })
+        .eq('title', item.title);
 
-    if (error) console.error(error);
-  });
+      if (error) console.error(error);
+    });
 
   return itemsWithImdbIds;
 };
 
-const getImdbId = async (catalogFromDB: NeedRating) => {
+const getImdbId = async (catalogFromDB: Awaited<NeedRating>) => {
   // need imdb id in order to get rating
   let itemsNoImdbId: NeedRating;
-  let itemsWithImdbIds: Promise<ImdbIdItem>[] = [];
+  let itemsWithImdbIds: ImdbIdItem[] = [];
   let dbItem: { title: string; imdbid: string | null };
   if (catalogFromDB) {
     itemsNoImdbId = catalogFromDB.filter((item) => !item.imdbid);
-    itemsWithImdbIds = itemsNoImdbId.map(async (itemsNoImdbId) => {
-      const encodedTitle = encodeURI(itemsNoImdbId.title);
-      const url = `https://imdb8.p.rapidapi.com/title/v2/find?title=${encodedTitle}`;
-      const titleSearch = await limiter.schedule(() => fetch(url, options));
-      const { results } = await titleSearch.json();
-      if (results?.length > 0) {
-        const { title, id } = results[0];
-        const checkTitleMatch = distance(itemsNoImdbId.title, title);
-        const formattedId = id.replace(/\D/g, ''); // '/title/tt12345678/' => '12345678'
-
-        dbItem =
-          checkTitleMatch <= 1
-            ? { title: title, imdbid: `tt${formattedId}` }
-            : { title: title, imdbid: null };
-      } else {
-        dbItem = { title: itemsNoImdbId.title, imdbid: null };
-      }
-      return dbItem;
-    });
+    itemsWithImdbIds = await Promise.all(
+      itemsNoImdbId.map(async (itemsNoImdbId) => {
+        try {
+          const encodedTitle = encodeURI(itemsNoImdbId.title);
+          const url = `https://imdb8.p.rapidapi.com/title/v2/find?title=${encodedTitle}`;
+          const titleSearch = await limiterImdbId.schedule(() =>
+            fetch(url, options)
+          );
+          const { results, totalMatches } = await titleSearch.json();
+          if (totalMatches > 0) {
+            const { title, id } = results[0];
+            const checkTitleMatch = distance(itemsNoImdbId.title, title);
+            const formattedId = id.replace(/\D/g, ''); // '/title/tt12345678/' => '12345678'
+            dbItem =
+              checkTitleMatch <= 1
+                ? { title: title, imdbid: `tt${formattedId}` }
+                : { title: title, imdbid: null };
+          } else {
+            dbItem = { title: itemsNoImdbId.title, imdbid: null };
+          }
+          return dbItem;
+        } catch {
+          console.log('Error: ', itemsNoImdbId);
+          return { title: itemsNoImdbId.title, imdbid: null };
+        }
+      })
+    ).then((imdbItems) => imdbItems.filter((item) => item.imdbid));
   }
-  const resolved = await Promise.all(itemsWithImdbIds.map((item) => item));
 
-  return resolved.filter((item) => item.imdbid);
+  return itemsWithImdbIds;
 };
 
 const extractImdbIds = (imdbItem?: ImdbIdItem[], dbItem?: NeedRating) => {
@@ -116,11 +127,11 @@ const extractImdbIds = (imdbItem?: ImdbIdItem[], dbItem?: NeedRating) => {
     });
   }
 
-  return [...imdbItemsArr, ...dbItemsArr];
+  return [...imdbItemsArr, ...dbItemsArr].filter((item) => item);
 };
 
 const getRating = async (
-  catalogFromDB: NeedRating,
+  catalogFromDB: Awaited<NeedRating>,
   newImdbIds: Awaited<ImdbIdItem>[]
 ) => {
   let itemsWithNewImdbId: Awaited<ImdbIdItem>[] = [];
@@ -132,6 +143,7 @@ const getRating = async (
     itemsNoRatings = catalogFromDB.filter((item) => item.imdbid);
 
     const imdbArr = extractImdbIds(itemsWithNewImdbId, itemsNoRatings);
+
     itemsWithRatings = await Promise.all(
       imdbArr.map(async (item) => {
         if (item) {
@@ -148,11 +160,54 @@ const getRating = async (
   return itemsWithRatings;
 };
 
-const addRatingsToDB = async (ratedItemsPromise: ImdbRatingItem[]) => {
+const addRatingsToDB = async (ratedItemsPromise: Awaited<ImdbRatingItem[]>) => {
   let ratedItems = ratedItemsPromise;
+  let itemsAddedToDb;
+  let itemsNotAddedToDb;
 
   try {
+    ratedItems = ratedItems.filter((item) => item);
     ratedItems = ImdbRatingItems.check(ratedItems);
+    if (ratedItems?.length > 0) {
+      ratedItems.map(async (item) => {
+        if (item.id) {
+          const id = item.id.replace(/\D/g, '');
+          const { error } = await supabaseService
+            .from('catalog')
+            .update({
+              rating: item.rating,
+            })
+            .eq('imdbid', `tt${id}`);
+
+          if (error) {
+            console.log('Error:', {
+              message: error.message,
+              details: error.details,
+            });
+          }
+        }
+
+        itemsAddedToDb = ratedItems
+          .map((item) => {
+            return {
+              title: item.title,
+              id: item.id,
+              rating: item.rating,
+            };
+          })
+          .filter((item) => item.rating);
+
+        itemsNotAddedToDb = ratedItems
+          .map((item) => {
+            return {
+              title: item.title,
+              id: item.id,
+              rating: item.rating,
+            };
+          })
+          .filter((item) => !item.rating);
+      });
+    }
   } catch (error) {
     if (error instanceof ValidationError)
       console.error('Error:', {
@@ -160,47 +215,6 @@ const addRatingsToDB = async (ratedItemsPromise: ImdbRatingItem[]) => {
         details: error.details,
       });
   }
-
-  if (ratedItems?.length > 0) {
-    ratedItems.map(async (item) => {
-      if (item.id) {
-        const id = item.id.replace(/\D/g, '');
-        const { error } = await supabaseService
-          .from('catalog')
-          .update({
-            rating: item.rating,
-          })
-          .eq('imdbid', `tt${id}`);
-
-        if (error) {
-          console.log('Error:', {
-            message: error.message,
-            details: error.details,
-          });
-        }
-      }
-    });
-  }
-
-  const itemsAddedToDb = ratedItems
-    .map((item) => {
-      return {
-        title: item.title,
-        id: item.id,
-        rating: item.rating,
-      };
-    })
-    .filter((item) => item.rating);
-
-  const itemsNotAddedToDb = ratedItems
-    .map((item) => {
-      return {
-        title: item.title,
-        id: item.id,
-        rating: item.rating,
-      };
-    })
-    .filter((item) => !item.rating);
 
   return {
     ratingsAdded: itemsAddedToDb,

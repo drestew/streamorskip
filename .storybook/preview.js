a535fdccc5f3dc6810f347e0835fd48a933cb752
drestew <drestew48@gmail.com>
2023-03-07 18:05:28 -0500

    Add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../styles/global-style';
+import { theme } from '../styles/theme';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle>
+        <Story />
+      </GlobalStyle>
+    </ThemeProvider>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};

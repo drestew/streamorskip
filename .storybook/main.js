ee35bc243f3460e423c7543e032f97ffacc3366b
drestew <drestew48@gmail.com>
2023-03-13 13:06:55 -0400

    Update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,21 @@
 module.exports = {
-  "stories": ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": {
-    name: "@storybook/nextjs",
-    options: {}
+  stories: [
+    '../features/**/*.mdx',
+    '../features/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react',
+    options: {},
+  },
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,15 +3,17 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../styles/global-style';
 import { theme } from '../styles/theme';
 
-export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle>
-        <Story />
-      </GlobalStyle>
-    </ThemeProvider>
-  ),
-];
+export default {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle>
+          <Story />
+        </GlobalStyle>
+      </ThemeProvider>
+    ),
+  ],
+};
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },

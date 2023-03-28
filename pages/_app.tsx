b43b0eea838292b9a031270369fe836205d23afd
drestew <drestew48@gmail.com>
2023-03-27 20:55:24 -0400

    Add user auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,16 +6,31 @@ import { GlobalStyle } from '@styles/global-style';
 import { theme } from '@styles/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 
 const queryClient = new QueryClient();
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </SessionContextProvider>
   );
 }

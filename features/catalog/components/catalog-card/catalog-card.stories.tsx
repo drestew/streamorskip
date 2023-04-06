e992ab30d9adbefb472be936466b0e480f3ad04f
drestew <drestew48@gmail.com>
2023-04-05 20:34:33 -0400

    Add stories for stream and skip rating

--- features/catalog/components/catalog-card/catalog-card.stories.tsx
+++ features/catalog/components/catalog-card/catalog-card.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { CatalogCard } from '@features/catalog';
 
@@ -10,13 +9,33 @@ export default {
 const Template: ComponentStory<typeof CatalogCard> = (args) => {
   return (
     <CatalogCard
-      title="Mighty Morphin Power Rangers"
-      synopsis="Five average teens are chosen by an intergalactic wizard to become the Power Rangers, who must use their new powers to fight the evil Rita Repulsa."
-      rating={6.4}
-      img="https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABVQvr2do2ukNtPmRSP3F5r0T_2TzFuPaYrYgL5du6wL2D3JvKPtySMSfYu9BEVuUJEmKaxHVx1mKWAxkMhm_rCRi1Q.jpg?r=04f"
+      title={args.title}
+      synopsis={args.synopsis}
+      rating={args.rating}
+      img={args.img}
+      stream={args.stream}
     />
   );
 };
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  title: 'Mighty Morphin Power Rangers',
+  synopsis:
+    'Five average teens are chosen by an intergalactic wizard to become the Power Rangers, who must use their new powers to fight the evil Rita Repulsa.',
+  rating: 6.4,
+  img: 'https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABVQvr2do2ukNtPmRSP3F5r0T_2TzFuPaYrYgL5du6wL2D3JvKPtySMSfYu9BEVuUJEmKaxHVx1mKWAxkMhm_rCRi1Q.jpg?r=04f',
+  stream: null,
+};
+
+export const Stream = Template.bind({});
+Stream.args = {
+  ...Default.args,
+  stream: true,
+};
+
+export const Skip = Template.bind({});
+Skip.args = {
+  ...Default.args,
+  stream: false,
+};

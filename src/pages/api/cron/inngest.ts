80819452d5942b267721144ab11ad0724b6ba1ad
drestew <drestew48@gmail.com>
2023-04-13 20:19:02 -0400

    Fix import for inngest

--- src/pages/api/cron/inngest.ts
+++ src/pages/api/cron/inngest.ts
@@ -1,6 +1,6 @@
 import { serve } from 'inngest/next';
 import { Inngest } from 'inngest';
-import getRatings from './cron/inngestFunctions/rating';
+import getRatings from './inngestFunctions/rating';
 import getGenres from './inngestFunctions/genre';
 
 export const inngest = new Inngest({ name: 'streamorskip' });

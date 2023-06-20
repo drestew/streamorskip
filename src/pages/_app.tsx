53ce38f23dfcd18154d9d28b94cdf48934c0f4da
drestew <drestew48@gmail.com>
2023-06-19 20:33:12 -0400

    Setup session context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,9 @@ import { theme } from '@styles/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import { Database } from '../types/supabase';
 
 const queryClient = new QueryClient();
 export default function App({
@@ -17,11 +18,11 @@ export default function App({
 }: AppProps<{
   initialSession: Session;
 }>) {
-  const [supabase] = useState(() => createBrowserSupabaseClient());
+  const [supabaseClient] = useState(() => createPagesBrowserClient<Database>());
 
   return (
     <SessionContextProvider
-      supabaseClient={supabase}
+      supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
       <ThemeProvider theme={theme}>

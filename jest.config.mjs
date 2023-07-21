caf9786e2ba55c0445d67a7b072e7df21a6b4a8f
drestew <drestew48@gmail.com>
2023-07-20 20:17:20 -0400

    Add custom render util for tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,11 +1,24 @@
-import nextJest from "next/jest";
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   dir: './',
-})
+});
 
-const config = {
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: [
+    'node_modules',
+    'utils',
+    '<rootDir>',
+  ],
   testEnvironment: 'jest-environment-jsdom',
-}
+  moduleNameMapper: {
+    '^@features/(.*)$': '<rootDir>/src/features/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@public/(.*)$': '<rootDir>/public/$1',
+  },
+};
 
-export default createJestConfig(config)
+export default createJestConfig(customJestConfig);

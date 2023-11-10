183448208e159a10d140863f8c9ff8b828842bdf
drestew <drestew48@gmail.com>
2023-11-09 21:00:13 -0500

    Filter content based on user settings

--- src/features/catalog/api/getCatalog.ts
+++ src/features/catalog/api/getCatalog.ts
@@ -1,4 +1,6 @@
 import { supabaseClient } from '@utils/supabase-client';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { Database } from '@src/types/supabase';
 
 async function searchGenre(genre: string) {
   const { data, error } = await supabaseClient
@@ -18,72 +20,134 @@ async function searchGenre(genre: string) {
 
   return Array.from(new Set(data?.map((item) => item.catalog_nfid)));
 }
+
+async function getProfileSettings(
+  userId: string | null,
+  supabase: SupabaseClient
+) {
+  if (!userId || userId === '') {
+    return null;
+  }
+
+  const { data, error } = await supabase
+    .from('profile')
+    .select('filter_rated, filter_saved, filter_removed_content')
+    .eq('id', userId)
+    .single();
+
+  if (error) {
+    console.log('Error getting profile settings:', {
+      message: error.message,
+      details: error.details,
+    });
+  }
+
+  return data;
+}
+
 export async function getCatalog(
   { pageParam = 0 },
   category: string,
   genre: string,
-  search: string
+  search: string,
+  userId: string | null,
+  supabase: SupabaseClient<Database>
 ) {
   const step = pageParam + 10;
-  const nfidSet = await searchGenre(genre);
+  const genreNfids = await searchGenre(genre);
+  const userSettings = await getProfileSettings(userId, supabase);
   let filteredData;
 
+  let query = supabaseClient
+    .from('catalog')
+    .select('nfid, title, img, synopsis, rating, vtype, on_Nflix')
+    .neq('rating', 0)
+    .eq('vtype', category)
+    .order('created_at', { ascending: false })
+    .range(pageParam, step);
+
   if (search) {
-    const { data, error } = await supabaseClient
-      .from('catalog')
-      .select('nfid, title, img, synopsis, rating, vtype, on_Nflix')
-      .is('on_Nflix', true)
-      .neq('rating', 0)
-      .ilike('title', `%${search}%`)
-      .range(pageParam, step);
-
+    query = query.ilike('title', `%${search}%`);
+    const { data, error } = await query;
     filteredData = data;
 
     if (error) {
-      console.log('Error:', {
+      console.log('Error getting searched content:', {
         message: error.message,
         details: error.details,
       });
     }
   }
 
-  if (nfidSet.length > 0 && !search) {
-    const { data, error } = await supabaseClient
-      .from('catalog')
-      .select('nfid, title, img, synopsis, rating, vtype, on_Nflix')
-      .is('on_Nflix', true)
-      .neq('rating', 0)
-      .eq('vtype', category)
-      .in('nfid', nfidSet)
-      .range(pageParam, step);
-
+  // when a genre is selected
+  if (genreNfids.length > 0 && !search) {
+    query = query.eq('vtype', category).in('nfid', genreNfids);
+    const { data, error } = await query;
     filteredData = data;
 
     if (error) {
-      console.log('Error: filter catalog genre', {
+      console.log('Error getting default catalog', {
         message: error.message,
         details: error.details,
       });
     }
   }
 
-  // to not add unfiltered catalog at the end of the filtered results
-  if (!search && nfidSet.length === 0) {
-    const { data, error } = await supabaseClient
-      .from('catalog')
-      .select('nfid, title, img, synopsis, rating, vtype, on_Nflix')
-      .is('on_Nflix', true)
-      .neq('rating', 0)
-      .eq('vtype', category)
-      .range(pageParam, step);
+  // default, also to not add unfiltered catalog at the end of the filtered results
+  if (!search && genreNfids.length === 0) {
+    // removed content is not shown by default unless set by user
+    if (!userSettings || userSettings.filter_removed_content) {
+      query = query.is('on_Nflix', true);
+    }
 
+    const { data, error } = await query;
     filteredData = data;
 
     if (error) {
-      console.log('Error: get catalog all titles', {
+      console.log('Error ending catalog after filtered results', {
         message: error.message,
         details: error.details,
       });
+    }
+  }
+
+  if (userSettings && !search) {
+    if (userSettings.filter_rated) {
+      const { data: ratedItems } = await supabase
+        .from('rating')
+        .select('catalog_item')
+        .eq('user_id', userId);
+
+      const ratedItemNfids = ratedItems?.map((nfid) => nfid.catalog_item);
+      query = query.not('nfid', 'in', `(${ratedItemNfids})`);
+      const { data, error } = await query;
+      filteredData = data;
+
+      if (error) {
+        console.log('Error filtering user rated items', {
+          message: error.message,
+          details: error.details,
+        });
+      }
+    }
+
+    if (userSettings.filter_saved) {
+      const { data: savedItems } = await supabase
+        .from('saved_list')
+        .select('catalog_item')
+        .eq('user_id', userId);
+
+      const savedItemNfids = savedItems?.map((nfid) => nfid.catalog_item);
+      query = query.not('nfid', 'in', `(${savedItemNfids})`);
+      const { data, error } = await query;
+      filteredData = data;
+
+      if (error) {
+        console.log('Error filtering user saved items', {
+          message: error.message,
+          details: error.details,
+        });
+      }
     }
   }
 

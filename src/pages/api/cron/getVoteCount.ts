46e0fb3db4da6cacfa8e14a20ca7c69bf1306072
drestew <drestew48@gmail.com>
2023-11-14 21:35:25 -0500

    Update vote count

--- src/pages/api/cron/getVoteCount.ts
+++ src/pages/api/cron/getVoteCount.ts
@@ -4,6 +4,13 @@ import { Env } from './index';
 import { Database } from '@src/types/supabase';
 import { ValidationError } from 'runtypes';
 
+type VoteCount = Awaited<{
+  imdbid: string | null;
+  rating: number | null;
+  voteCount: number | null;
+  newVoteCount: number | null;
+}>;
+
 async function getRatingsFromDB(supabase: SupabaseClient<Database>) {
   const { data, error } = await supabase
     .from('catalog')
@@ -28,11 +35,7 @@ async function getRatingCountsFromImdb(
   catalogFromDB: { imdbid: string | null; rating: number | null }[] | null,
   imdbKey: string
 ) {
-  let itemsWithVoteCount: Awaited<{
-    imdbid: string | null;
-    rating: number | null;
-    voteCount: number | null;
-  }>[] = [];
+  let itemsWithVoteCount: VoteCount[] = [];
 
   if (catalogFromDB && catalogFromDB.length > 0) {
     itemsWithVoteCount = await Promise.all(
@@ -52,6 +55,7 @@ async function getRatingCountsFromImdb(
         return {
           ...item,
           voteCount: Number(imdbItem.totalRatingVotes) || null,
+          newVoteCount: null,
         };
       })
     );
@@ -62,12 +66,48 @@ async function getRatingCountsFromImdb(
   );
 }
 
+function modifyVoteCount(voteCount: number | null) {
+  if (!voteCount || voteCount === 0) {
+    return null;
+  }
+
+  const voteCountStr = voteCount.toString();
+  let newVoteCount = null;
+
+  if (voteCountStr.length === 5) {
+    const firstNumber = getRandomIntInclusive(7, 9);
+    newVoteCount = voteCountStr.substring(0, voteCountStr.length - 2);
+    newVoteCount = `${firstNumber}${newVoteCount}`;
+  } else if (voteCountStr.length === 6 && voteCount < 500000) {
+    newVoteCount = `1${voteCountStr.substring(1, 5)}`;
+  } else if (voteCountStr.length === 6 && voteCount < 999999) {
+    newVoteCount = `2${voteCountStr.substring(1, 5)}`;
+  } else if (voteCountStr.length > 6) {
+    newVoteCount = `3${voteCountStr.substring(1, 5)}`;
+  }
+
+  return Number(newVoteCount) || voteCount;
+}
+
+function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function updateVoteCount(voteCounts: VoteCount[]) {
+  return voteCounts.map((item) => {
+    const newVoteCount = modifyVoteCount(item.voteCount || null);
+    return { ...item, newVoteCount };
+  });
+}
+
 const getVoteCount = {
   async fetch(req: Request, env: Env, supabase: SupabaseClient<Database>) {
     const ratings = await getRatingsFromDB(supabase);
     const ratingCounts = await getRatingCountsFromImdb(ratings, env.IMDB_KEY);
-
-    return new Response(JSON.stringify(ratingCounts), {
+    const updatedCounts = updateVoteCount(ratingCounts);
+    return new Response(JSON.stringify(updatedCounts), {
       headers: {
         'Content-Type': 'application/json',
       },

b368a686ac31aa818ae07f65d1a9fc91ff848688
drestew <drestew48@gmail.com>
2023-12-02 21:53:18 -0500

    Replace children prop with conditional component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,5 @@
-import { LogInForm } from '@features/auth';
 import { Layout } from '@features/auth';
 
 export default function LogIn() {
-  return (
-    <Layout>
-      <LogInForm />
-    </Layout>
-  );
+  return <Layout />;
 }
